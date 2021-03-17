253a2ceb41be7ef5c4931e78401ced1e6494181f
Moklesur Rahman <developer.moklesur@gmail.com>
2021-03-17 15:38:48 +0600

    Update now.js

--- now/now.js
+++ now/now.js
@@ -1,24 +1,12 @@
-let sum = (a,b) => {
-    return a + b
-}
+let numbers = [1,2,3,4,5]
 
-let sum1 = (a,b) => {
-    return a * b
-}
 
-let sum2 = (a,b) => {
-    return a - b
+for(num of numbers){
+    numbers.push(num * 2)
 }
-// console.log(sum(1,2))
-// console.log(sum1(1,2))
+console.log(numbers)
 
-let arr = [sum, sum1, sum2]
-let a = 2
-    b = 3
-for(i = 0; i < arr.length; i++){
-    end = arr[i](a,b)
-    console.log(end)
-}
+
 
 
 
