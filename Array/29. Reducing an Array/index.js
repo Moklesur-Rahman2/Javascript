aee7998ab2c575520fc7f3bf72f543bc006bc78c
Moklesur Rahman <developer.moklesur@gmail.com>
2021-03-18 13:45:26 +0600

    update

--- Array/29. Reducing an Array/index.js
+++ Array/29. Reducing an Array/index.js
@@ -1,14 +1,6 @@
-let arr = [3,5,6];
-
-//let sum = 0;
-
-// sum = sum + num
-console.log(arr.reduce( (jog,num) => {
-return jog + num;
-},0))
-
+let arr = [5,10, 2,5]
 
 let newArr = arr.reduce((value, index, arr) => {
-    return value * index
+    return value + index
 })
 console.log(newArr)
--- Array/30. When to Use Map, Filter, _ Reduce/index.js
+++ Array/30. When to Use Map, Filter, _ Reduce/index.js
@@ -9,18 +9,29 @@ const Fighter = [
 
 // Problem
 // 1. Only Pick their names
+let fighterName = Fighter.map((value) => {
+    return value.name
+})
+console.log(fighterName)
+
 // 2. Pick the fighter whose combatskill >= 80
+let skillScore = Fighter.filter((value) => {
+    return value.combatskill >= 80
+})
+console.log(skillScore)
+
 // 3. sum of all combatSkills
 
 
-// ['john','mary','taylor']
 
-console.log(Fighter.map( (f) => f.name ).filter( (f) => f.combatskill >= 80 ).reduce( (sum, f) => sum + f.combatskill, 0 ));
+// // ['john','mary','taylor']
 
-
-
-// console.log(Fighter.filter( (f) => f.combatskill >= 80 ));
+// console.log(Fighter.map( (f) => f.name ).filter( (f) => f.combatskill >= 80 ).reduce( (sum, f) => sum + f.combatskill, 0 ));
 
 
 
-// console.log(Fighter.reduce( (sum, f) => sum + f.combatskill, 0 ));
+// // console.log(Fighter.filter( (f) => f.combatskill >= 80 ));
+
+
+
+// // console.log(Fighter.reduce( (sum, f) => sum + f.combatskill, 0 ));
