3798337c01f5c1d261d3b70f965288f0c99b7e26
Moklesur Rahman <developer.moklesur@gmail.com>
2021-04-01 15:21:51 +0600

    update

--- JavaScript Other ES6, ES7, and ES8 Features/3. Data Structure - Set/index.js
+++ JavaScript Other ES6, ES7, and ES8 Features/3. Data Structure - Set/index.js
@@ -1,14 +1,28 @@
 
-let mySet = new Set()
-mySet.add(100)
-mySet.add(200)
-mySet.add(300)
-mySet.add(100)
-mySet.add(700)
-
-mySet.add(100)
+let myArray = [1,2,3,4,5]
 
 
-console.log(mySet.size)
+
+let mySet = new Set(myArray)
+
+for(let value of mySet){
+    console.log(value)
+}
+// console.log(mySet)
+
+console.log([...mySet])
 
 
+// union
+
+let setA = [1,2,3,4,5]
+let setB = [2,6,4,7,5]
+
+let union = new Set([...setA,...setB])
+console.log(union)
+
+
+// intersection
+
+let intersection = new Set([...setA].filter(value => [...setB].has(value)));
+console.log(intersection)
